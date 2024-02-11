6dae1a66f5ea8153dc4960a114a9d361ad6e6a0a
Niko Diamadis <niko@cyb3rko.de>
2024-02-11 01:05:35 +0100

    Add copyright notice and license

--- lib/src/main/cpp/jabreader.h
+++ lib/src/main/cpp/jabreader.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2024 Cyb3rKo
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Lesser Public License for more
+ * details.
+ *
+ * A copy of the GNU Lesser General Public License can be found alongside this
+ * library's source code. Alternatively, see at <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef JAB_CODE_READER_JABREADER_H
 #define JAB_CODE_READER_JABREADER_H
 
